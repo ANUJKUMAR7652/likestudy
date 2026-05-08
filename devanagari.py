0edb307d2c714e20ee0891415b6da43c22fd748f
ANUJKUMAR7652 <64329462+ANUJKUMAR7652@users.noreply.github.com>
2026-05-08 12:09:44 +0530

    Update devanagari.py

--- devanagari.py
+++ devanagari.py
@@ -1,4 +1,4 @@
-ltw1import urllib.request
+import urllib.request
 print("Naya Hindi font download ho raha hai...")
 # Lohit Devanagari font (Bahut stable hai)
 url = "https://github.com/fedora-hindi/lohit-hindi/raw/master/Lohit-Hindi.ttf"
