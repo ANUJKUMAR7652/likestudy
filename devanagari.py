9a2cba8148a08482c6927009123120d9fa24db10
ANUJKUMAR7652 <64329462+ANUJKUMAR7652@users.noreply.github.com>
2026-05-09 12:08:34 +0530

    Delete devanagari.py

--- devanagari.py
+++
@@ -1,6 +0,0 @@
-import urllib.request
-print("Naya Hindi font download ho raha hai...")
-# Lohit Devanagari font (Bahut stable hai)
-url = "https://github.com/fedora-hindi/lohit-hindi/raw/master/Lohit-Hindi.ttf"
-urllib.request.urlretrieve(url, "devanagari.ttf")
-print("✅ 'devanagari.ttf' mil gayi!")
